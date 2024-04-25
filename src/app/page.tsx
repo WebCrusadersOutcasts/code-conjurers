191e1f3eeb4f3dd76dc2f8a67a7346196677caa0
kennethjason07 <kenj7214@gmail.com>
2024-04-25 18:53:55 +0530

    18:55

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import Link from "next/link";
 import ToolsCard from "./components/Tools";
 import { Grid, Typography } from "@mui/material";
@@ -16,42 +15,38 @@ export default function Home() {
   return (
     <>
       <style jsx>{`
-        @media (max-width: 960px) {
-          /* Styles for tablets */
-          header {
-            height: 100px;
-          }
-        }
-
-        @media (max-width: 600px) {
-          /* Styles for mobile devices */
-          header {
-            height: 80px;
-          }
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          min-height: 100vh;
         }
       `}</style>
-      <header style={{ padding: "10px 3px", textAlign: "center", background: "#35353f", color: "white", display: "flex", alignItems: "center", justifyContent: "center" }}>
-        {/* <img src="/images/logo.png" alt="Logo" style={{ width: "100px", height: "auto", marginRight: "10px" }} /> */}
-        <Typography variant="h3" style={{ margin: "5" }}>Predicting House Prices</Typography>
-      </header>
-      <Grid container spacing={2} style={{ padding: "20px" }}>
-        {toolsList.map((el, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Link href={el.link}>
-              <div style={{ cursor: "pointer", display: "flex", flexDirection: "column", height: "100%" }}>
-                <ToolsCard
-                  name={el.name}
-                  description={el.description}
-                  image={el.image}
-                />
-              </div>
-            </Link>
-          </Grid>
-        ))}
-      </Grid>
-      <footer style={{ padding: "20px", textAlign: "center", background: "black", color: "white" }}>
-        <Typography variant="body1">&copy; Made By Web Crusaders </Typography>
-      </footer>
+      <div className="container">
+        <header style={{ padding: "10px 3px", textAlign: "center", background: "#35353f", color: "white", display: "flex", alignItems: "center", justifyContent: "center" }}>
+          {/* <img src="/images/logo.png" alt="Logo" style={{ width: "100px", height: "auto", marginRight: "10px" }} /> */}
+          <Typography variant="h3" style={{ margin: "5" }}>Predicting House Prices</Typography>
+        </header>
+        <Grid container spacing={2} style={{ padding: "20px" }}>
+          {toolsList.map((el, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Link href={el.link}>
+                <div style={{ cursor: "pointer", display: "flex", flexDirection: "column", height: "100%" }}>
+                  <ToolsCard
+                    name={el.name}
+                    description={el.description}
+                    image={el.image}
+                  />
+                </div>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+        <footer style={{ padding: "20px", textAlign: "center", background: "black", color: "white" }}>
+          <Typography variant="body1">&copy; Made By Web Crusaders </Typography>
+        </footer>
+      </div>
     </>
   );
 }
